990a14b028ce5f96814d721c6ccd7553188ab7dd
lmbelo <lucas.belo@live.com>
2025-02-01 14:01:03 -0300

    Chat

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -16,7 +16,7 @@ const Conversation: React.FC<ConversationProps> = ({
 }) => {
     const router = useRouter();
     const otherUser = conversation.users.find((user) => user.id != userId)
-    console.log("conversation", otherUser)
+
     return (
         <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
             <p className="mb-6 text-xl">{otherUser?.name}</p>    
